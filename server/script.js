f5fa479d652fcdcad3eb94a5aa740402bf159e73
Manoel Rocha <manoelmelo367@gmail.com>
2023-08-14 17:06:02 -0300

    test: juntado todos num arquivo

--- server/script.js
+++ server/script.js
@@ -104,3 +104,58 @@ readData('ir.csv')
   .catch((error) => {
     console.error('Ocorreu um erro:', error);
   });
+
+  const findLargestNumberInFile = async (filePath) => {
+    let largestNumber = 0;
+  
+    const fileStream = fs.createReadStream(filePath);
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity,
+    });
+  
+    for await (const line of rl) {
+      const endTime = parseInt(line.trim(), 10);
+      if (!isNaN(endTime) && endTime > largestNumber) {
+        largestNumber = endTime;
+      }
+    }
+  
+    return largestNumber;
+  };
+
+   const processEndTimes = async (filePath, databaseStartupFilePath, databaseRecoveryFilePath, otherElementsFilePath) => {
+    const findLargestNumber = await findLargestNumberInFile(filePath) + 1;
+  
+    const x = Array.from({ length: findLargestNumber }, (_, i) => i);
+    const y = Array(x.length).fill(0);
+  
+    const fileStream = fs.createReadStream(filePath);
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity,
+    });
+  
+    let j = 0;
+    for await (const line of rl) {
+      const endTime = parseInt(line.trim(), 10);
+      if (!isNaN(endTime)) {
+        while (x[j] < endTime) {
+          j++;
+        }
+        if (x[j] === endTime) {
+          y[j] += 1;
+        }
+      }
+    }
+  
+    return { x, y};
+  };
+  const {x, y} = await processEndTimes(
+    './temp/end_time.txt',
+    './temp/database_startup.txt',
+    './temp/database_recovery.txt',
+    './temp/other_elements.txt'
+  );
+
+  console.log(x);

a2c7eaf5388060965349f2bfa458da3a40b745ce
Airck Lima <aircklima@gmail.com>
2023-11-27 16:52:01 -0300

    aprimoração dos cards de opções

--- server/web/src/componentes/FormController.tsx
+++ server/web/src/componentes/FormController.tsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 import CpuOptions from "./CpuOptions";
 import TransferOptions from "./TrasferOptions";
 
-
 interface FormProp {
     initServer: (params: Array<string>) => void;
 }
 
 const FormController = ({ initServer }: FormProp) => {
-
-    const [cpuOptionsVisibility, setCpuOptionsVisibility] = useState<boolean>(false)
-    const [transferOptionsVisibility, setTransferOptionsVisibility] = useState<boolean>(false)
+    const [cpuOptionsVisibility, setCpuOptionsVisibility] =
+        useState<boolean>(false);
+    const [transferOptionsVisibility, setTransferOptionsVisibility] =
+        useState<boolean>(false);
 
     const showCpuOptions = (event: any) => {
-        setCpuOptionsVisibility(!cpuOptionsVisibility)
-    }
+        setCpuOptionsVisibility(!cpuOptionsVisibility);
+    };
 
     const showTransferOptions = (event: any) => {
-        setTransferOptionsVisibility(!transferOptionsVisibility)
-    }
+        setTransferOptionsVisibility(!transferOptionsVisibility);
+    };
 
     function handleSubmit(e: any) {
         e.preventDefault();
@@ -32,90 +32,66 @@ const FormController = ({ initServer }: FormProp) => {
     }
 
     return (
-        <div>
-            <div className="container form-controller-panel">
-                <div className="card">
-                    <h3 className="card-header text-light">MM-DIRECT</h3>
-                    <div className="card-body">
-                        <form method="post" onSubmit={handleSubmit}>
-                            <div className="row justify-content-center text-center">
-                                <div className="col-auto">
-
-                                    <p className="display-6">GRAFICOS </p>
-                                    <div className="row mb-3">
-                                        <div className="col-auto">
-                                            <div className="cpu-select">
-                                                <input
-                                                    onClick={showCpuOptions}
-                                                    type="checkbox"
-                                                    name="graph-cpu"
-                                                    className="cpu-checkbox me-1"
-                                                />
-                                                <label>
-                                                    CPU
-                                                </label>
-                                                {
-                                                    cpuOptionsVisibility && <CpuOptions />
-                                                }
-                                            </div>
-                                        </div>
-                                        <div className="col-auto">
+        <div className="container form-controller-panel">
+            <div className="card">
+                <h3 className="card-header text-light">MM-DIRECT</h3>
+                <div className="card-body">
+                    <form method="post" onSubmit={handleSubmit}>
+                        <div className="row justify-content-center text-center">
+                            <div className="col-auto">
+                                <p className="display-6">GRÁFICOS </p>
+                                <div className="row">
+                                    <div className="col-auto">
+                                        <div className="cpu-select">
                                             <input
-                                                onClick={showTransferOptions}
+                                                onClick={showCpuOptions}
                                                 type="checkbox"
-                                                name="graph-transf"
-                                                className="me-1"
+                                                name="graph-cpu"
+                                                className="cpu-checkbox me-1"
                                             />
-                                            <label>
-                                                TRANSIÇÕES
-                                            </label>
-                                            {
-                                                transferOptionsVisibility && <CpuOptions />
-                                            }
+                                            <label className="h5">CPU</label>
                                         </div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <input
+                                            onClick={showTransferOptions}
+                                            type="checkbox"
+                                            name="graph-transf"
+                                            className="me-1"
+                                        />
+                                        <label className="h5">TRANSIÇÕES</label>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
-                            {/* <div className="col-3">
-                <select className="form-select" name="drop">
-                    <option selected>PARÂMETROS</option>
-                    <option value="1">Opção 1</option>
-                    <option value="2">Opção 2</option>
-                    <option value="3">Opção 3</option>
-                </select>
-            </div>
-
-
-            <div className="col-4">
-                <label htmlFor="customRange2" className="form-label">Range</label>
-                <input type="range" className="form-range" min="0" max="5" id="customRange2" />
-            </div>
-
-            <div className="col-auto">
-                <input className="form-check-input" type="checkbox" name="check-1" />
-                <label className="form-check-label me-3"> Opção 1 </label>
-                <input className="form-check-input" type="checkbox" name="check-2" />
-                <label className="form-check-label me-3"> Opção 2 </label>
-                <input className="form-check-input" type="checkbox" name="check-3" />
-                <label className="form-check-label me-3"> Opção 3 </label>
-            </div> */}
-                            <div className=" row justify-content-center panel-buttons">
-                                <div className="col-auto">
-                                    <button
-                                        className="btn btn-primary me-3"
-                                        type="reset"
-                                    >
-                                        Reset
-                                    </button>
-                                    <button type="submit" className="btn btn-success">
-                                        Start
-                                    </button>
-                                </div>
+                        <div className="row justify-content-center mb-3">
+                            <div className="col-auto">
+                                {cpuOptionsVisibility && <CpuOptions />}
                             </div>
-                            {/* <button onClick={()=>{initServer('')}} className="btn btn-success me-3">Start</button> */}
-                        </form>
-                    </div>
+                            <div className="col-auto">
+                                {transferOptionsVisibility && <TransferOptions />}
+                            </div>
+                        </div>
+
+                        <div className=" row justify-content-center panel-buttons">
+                            <div className="col-auto">
+                                <button
+                                    className="btn btn-primary me-3"
+                                    type="reset"
+                                >
+                                    Reset
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="btn btn-success"
+                                >
+                                    Start
+                                </button>
+                            </div>
+                        </div>
+                        {/* <button onClick={()=>{initServer('')}} className="btn btn-success me-3">Start</button> */}
+                    </form>
                 </div>
             </div>
         </div>

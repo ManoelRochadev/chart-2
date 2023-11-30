e423cf2a9054c91f83cf3778dd95e5e2d6b7b8c7
Manoel Rocha <manoelmelo367@gmail.com>
2023-11-30 16:09:11 -0300

    NEW layout

--- web/src/componentes/FormController.tsx
+++ web/src/componentes/FormController.tsx
@@ -1,63 +1,102 @@
-import StartButton from "./StartButton";
+import { useState } from "react";
+import CpuOptions from "./CpuOptions";
+import TransferOptions from "./TrasferOptions";
 
 interface FormProp {
-    initServer: () => void;
+    initServer: (params: Array<string>) => void;
 }
 
 const FormController = ({ initServer }: FormProp) => {
+    const [cpuOptionsVisibility, setCpuOptionsVisibility] =
+        useState<boolean>(false);
+    const [transferOptionsVisibility, setTransferOptionsVisibility] =
+        useState<boolean>(false);
+
+    const showCpuOptions = () => {
+        setCpuOptionsVisibility(!cpuOptionsVisibility);
+    };
+
+    const showTransferOptions = () => {
+        setTransferOptionsVisibility(!transferOptionsVisibility);
+    };
+
     function handleSubmit(e: any) {
         e.preventDefault();
 
         const form = e.target;
         const formData = new FormData(form);
 
-        fetch('/some-api', { method: form.method, body: formData });
-
         const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson);
+        console.log(...Object.keys(formJson));
+        initServer(Object.keys(formJson));
     }
 
     return (
-        <form method="post" onSubmit={handleSubmit}>
-            <p>
-                GRAFICOS: <br />
-                <label className="me-3">
-                    <input type="checkbox" name="graph-1" /> CPU
-                </label>
-                <label className="me-3">
-                    <input type="checkbox" name="graph-2" /> TRANSIÇÕES
-                </label>
-            </p>
-            <hr />
-            <div className="col-3">
-                <select className="form-select" name="drop">
-                    <option selected>PARÂMETROS</option>
-                    <option value="1">Opção 1</option>
-                    <option value="2">Opção 2</option>
-                    <option value="3">Opção 3</option>
-                </select>
-            </div>
-            <hr />
+        <div className="container form-controller-panel">
+            <div className="card">
+                <h3 className="card-header text-light">MM-DIRECT</h3>
+                <div className="card-body">
+                    <form method="post" onSubmit={handleSubmit}>
+                        <div className="row justify-content-center text-center">
+                            <div className="col-auto">
+                                <p className="display-6">GRÁFICOS </p>
+                                <div className="row">
+                                    <div className="col-auto">
+                                        <input
+                                            id="check-cpu"
+                                            onClick={showCpuOptions}
+                                            type="checkbox"
+                                            name="graph-cpu"
+                                            className="btn btn-check cpu-checkbox me-1"
+                                            autoComplete="off"
+                                        />
+                                        <label className="h5 border border-2 border-info rounded p-1" htmlFor="check-cpu">CPU</label>
+                                    </div>
+                                    <div className="col-auto">
+                                        <input
+                                            id="check-transfer"
+                                            onClick={showTransferOptions}
+                                            type="checkbox"
+                                            name="graph-transf"
+                                            className="btn btn-check me-1"
+                                            autoComplete="off"
+                                        />
+                                        <label className="h5 border border-2 border-info rounded p-1" htmlFor="check-transfer">TRANSIÇÕES</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-            <div className="col-4">
-                <label htmlFor="customRange2" className="form-label">Range</label>
-                <input type="range" className="form-range" min="0" max="5" id="customRange2" />
+                        <div className="row justify-content-center mb-3">
+                            <div className="col-auto">
+                                {cpuOptionsVisibility && <CpuOptions />}
+                            </div>
+                            <div className="col-auto">
+                                {transferOptionsVisibility && <TransferOptions />}
+                            </div>
+                        </div>
 
+                        <div className=" row justify-content-center panel-buttons">
+                            <div className="col-auto">
+                                <button
+                                    className="btn btn-primary me-3"
+                                    type="reset"
+                                >
+                                    Reset
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="btn btn-success"
+                                >
+                                    Start
+                                </button>
+                            </div>
+                        </div>
+                        {/* <button onClick={()=>{initServer('')}} className="btn btn-success me-3">Start</button> */}
+                    </form>
+                </div>
             </div>
-
-            <hr />
-            <div className="col-auto">
-                <input className="form-check-input" type="checkbox" name="check-1" />
-                <label className="form-check-label me-3"> Opção 1 </label>
-                <input className="form-check-input" type="checkbox" name="check-2" />
-                <label className="form-check-label me-3"> Opção 2 </label>
-                <input className="form-check-input" type="checkbox" name="check-3" />
-                <label className="form-check-label me-3"> Opção 3 </label>
-            </div>
-            <hr />
-            <button className="btn btn-primary me-3" type="reset">Reset</button>
-            <button onClick={initServer} className="btn btn-success me-3">Start</button>
-        </form>
+        </div>
     );
-}
+};
 export default FormController;
